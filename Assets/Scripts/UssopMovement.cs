dd3cea1de73678d7756e57bef1a7b03d0c1cdd48
Rockolosus33 <jbasart9@gmail.com>
2024-04-16 12:23:02 +0200

    movment fixed
    he rebentao el code i ahora furula, chachi pistachi

--- Assets/Scripts/UssopMovement.cs
+++ Assets/Scripts/UssopMovement.cs
@@ -6,42 +6,33 @@ public class UssopMovement : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
     private float horizontal;
-    public float moveSpeed = 10f;
+    public float moveSpeed;
     public Vector2 direction;
     private bool facingRight = true;
-    public float maxSpeed = 7f;
-    public float linearDrag = 4f;
+    public float maxSpeed;
+    public float linearDrag;
     public Animator animator;
+    public Transform position;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 Movementnew = new Vector3(horizontal,0,0);
+
+        transform.position += Movementnew * moveSpeed * Time.deltaTime;
+
     }
 
-    private void FixedUpdate()
-    {
-        rigidbody2D.velocity = new Vector2 (horizontal, rigidbody2D.velocity.y);
-    }
-
-    void moveCharacter(float horizontal)
-    {
-    rigidbody2D.AddForce(Vector2.right * horizontal * moveSpeed);
-
-    if((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight)){
-        Flip();
-    }
-    if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed){
-        rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
-    }
-    animator.SetFloat("horizontal", Mathf.Abs(rigidbody2D.velocity.x));
-    }
 
     void Flip()
     {
