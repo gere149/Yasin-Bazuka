3069af61d4c38a4b7b99117d5b93d4f9b2037222
gere149 <gere.delatorre@gmail.com>
2024-06-01 18:24:44 +0200

    Barra de Vida Jugador
    Barra de vida Jugador completada!!!

--- Assets/Scripts/Scripts/Jugador/VidaDelJugador.cs
+++ Assets/Scripts/Scripts/Jugador/VidaDelJugador.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class VidaDelJugador : MonoBehaviour
 {
     [SerializeField] private float vida;
+    [SerializeField] private BarraDeVida barraDeVida;
+
+    private void Start()
+    {
+        barraDeVida.InicializarBarraDeVida(vida);
+    }
 
     public void TomarDaño(float daño)
     {
         vida -= daño;
+
+        barraDeVida.CambiarVidaActual(vida);
 
         if(vida <= 0)
         {
