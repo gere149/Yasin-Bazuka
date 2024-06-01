e4ec32581dda826b59660769382e51a567625e27
gere149 <gere.delatorre@gmail.com>
2024-06-01 12:50:39 +0200

    Arreglando scirpt hud enemigo
    probando

--- Assets/Scripts/Enemigo/VidaEnemigo.cs
+++ Assets/Scripts/Enemigo/VidaEnemigo.cs
@@ -1,16 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VidaEnemigo : MonoBehaviour
 {
+    /*public event Action HealthChanged;
+    [SerializeField]
+    private int minHealth = 0;
+    [SerializeField]
+    private int maxHealth = 100;
+    private int currentHealth;
 
+    public int CurrentHealth { get => currentHealth; set => currentHealth = value; }
+    public int MinHealth => minHealth;
+    public int MaxHealth => maxHealth;*/
     [SerializeField] private float vida;
-    private Animator ani;
+    private Animator animator;
+    private Transform playerTransform;
+
     void Start()
     {
-        ani = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
+
+    /*public void Decrement(int amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, minHealth, maxHealth);
+    }*/
 
     public void TomarDaño(float daño)
     {
@@ -24,15 +44,8 @@ public class VidaEnemigo : MonoBehaviour
     
     private void Muerte()
     {
-        if(vida <= 0)
-        {
-            ani.SetBool("Muerte", true);
-            
-        }
-        else
-        {
-            ani.SetBool("Muerte", false);
-        }
+        animator.SetTrigger("Muerte");
+        Destroy(gameObject);
     }
 
 }
