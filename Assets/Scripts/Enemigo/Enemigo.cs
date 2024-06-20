be8c01253f9173f4b3d37d4b005b4f6a2ebab294
gere149 <gere.delatorre@gmail.com>
2024-06-20 13:17:13 +0200

    Limpeiza codigo comentado //
    Codigo mas limpio con menos lineas.

--- Assets/Scripts/Enemigo/Enemigo.cs
+++ Assets/Scripts/Enemigo/Enemigo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemigo : MonoBehaviour
-{
+{   
     private Animator animator;
     public Rigidbody2D rb2D;
     private bool mirandoDerecha = true;
@@ -39,91 +39,4 @@ public class Enemigo : MonoBehaviour
             transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
         }
     }
-
-
-
-
-    /*private Animator animator;
-    public Rigidbody2D rb2D;
-    public Transform jugador;
-    private bool mirandoDerecha = true;
-
-    [Header("Vida")]
-    [SerializeField] private float vida;
-    private float vidaMax = 100;
-    public SpriteRenderer healthBar;
-    public Transform healthBarTransform;
-
-    [Header("Ataque")]
-    [SerializeField] private Transform controladorAtaque;
-    [SerializeField] private float radioAtaque;
-    [SerializeField] private float dañoAtaque;
-
-
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-        rb2D = GetComponent<Rigidbody2D>();
-        jugador = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
-
-    private void Update()
-    {
-        float distanciaJugador = Vector2.Distance(transform.position, jugador.position);
-        animator.SetFloat("distanciaJugador", distanciaJugador);
-
-        ActualizarBarraDeVida(vida);
-    }
-
-    public void TomarDaño(float daño)
-    {
-        vida -= daño;
-
-        if(vida <= 0)
-        {
-            animator.SetTrigger("Muerte 0");
-            Muerte(1);
-        }
-    }
-
-    private void Muerte(int time)
-    {
-        Destroy(gameObject, time);
-    }
-
-    public void MirarJugador()
-    {
-        if((jugador.position.x > transform.position.x && !mirandoDerecha) || (jugador.position.x < transform.position.x && mirandoDerecha))
-        {
-            mirandoDerecha = !mirandoDerecha;
-            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
-        }
-    }
-
-    //Intentar usar a partir de aqui el codigo y crear un Ataque general que sirva para CombateCac de player y Enemigo de enemy.
-    public void Ataque()
-    {
-        Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorAtaque.position, radioAtaque);
-
-        foreach(Collider2D colision in objetos)
-        {
-            if(colision.CompareTag("Player"))
-            {
-                colision.GetComponent<VidaDelJugador>().TomarDaño(dañoAtaque);
-            }
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(controladorAtaque.position, radioAtaque);
-    }
-
-    private void ActualizarBarraDeVida(float porcentajeVida)
-    {
-        porcentajeVida = (float)vida / vidaMax;
-        healthBarTransform.transform.localScale = new Vector3(porcentajeVida * 0.5f, 0.06810274f, 1f);
-        healthBar.color = Color.Lerp(Color.red, Color.green, porcentajeVida);
-    }*/
 }
